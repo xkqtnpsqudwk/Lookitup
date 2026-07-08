7af0f6c9b433b61f7c5637400a646f6cefcfd406
DayTomer <xkqtnpsqudwk9649@gmail.com>
2026-07-08 23:49:59 +0200

    Make Android debug build runnable

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
     id("org.jetbrains.kotlin.plugin.serialization")
 }
@@ -15,7 +14,7 @@ val configuredApiBaseUrl = providers
 
 android {
     namespace = "com.lookitup.mobile"
-    compileSdk = 36
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.lookitup.mobile"
@@ -34,7 +33,7 @@ android {
 }
 
 dependencies {
-    implementation(platform("androidx.compose:compose-bom:2026.04.00"))
+    implementation(platform("androidx.compose:compose-bom:2026.06.01"))
     implementation("androidx.activity:activity-compose:1.12.0")
     implementation("androidx.compose.foundation:foundation")
     implementation("androidx.compose.material3:material3")
